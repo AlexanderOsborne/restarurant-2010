67e9ef189f4092d336de2cad624f12eab3d5922e
awosborn <alanturing@example.coclear>
2020-10-07 13:40:34 -0600

    Passes all test except closing_time

---
+++ lib/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant
+
+attr_reader :name, :dishes, :opening_time, :closing_time
+
+  def initialize(opening_time, name, dishes = [])
+    @opening_time = opening_time
+    @name = name
+    @closing_time = closing_time
+  end
+end
